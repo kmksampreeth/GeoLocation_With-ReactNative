a12217be4487b00ce804e229d6d0582f71514661
kmksampreeth <kmksampreeth@gmail.com>
2020-06-04 10:09:38 +0530

    kmksampreeth

---
+++ App.js
@@ -0,0 +1,132 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  PermissionsAndroid,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+import Geolocation from '@react-native-community/geolocation';
+
+
+
+export default class App extends React.Component {
+  state = {
+    currentLongitude: 'unknown',//Initial Longitude
+    currentLatitude: 'unknown',//Initial Latitude
+ }
+ componentDidMount = () => {
+  let that =this;
+  //Checking for the permission just after component loaded
+  if(Platform.OS === 'ios'){
+    this.callLocation(that);
+  }else{
+    async function requestLocationPermission() {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,{
+            'title': 'Location Access Required',
+            'message': 'This App needs to Access your location'
+          }
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          //To Check, If Permission is granted
+          that.callLocation(that);
+        } else {
+          alert("Permission Denied");
+        }
+      } catch (err) {
+        alert("err",err);
+        console.warn(err)
+      }
+    }
+    requestLocationPermission();
+  }    
+ }
+
+ callLocation(that){
+   
+  //alert("callLocation Called");
+    Geolocation.getCurrentPosition(
+      
+      //Will give you the current location
+       (position) => {
+          const currentLongitude = JSON.stringify(position.coords.longitude);
+          //getting the Longitude from the location json
+          const currentLatitude = JSON.stringify(position.coords.latitude);
+          //getting the Latitude from the location json
+          that.setState({ currentLongitude:currentLongitude });
+          //Setting state Longitude to re re-render the Longitude Text
+          that.setState({ currentLatitude:currentLatitude });
+          //Setting state Latitude to re re-render the Longitude Text
+       },
+       (error) => alert(error.message),
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    that.watchID = Geolocation.watchPosition((position) => {
+      //Will give you the location on location change
+        console.log(position);
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        //getting the Longitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        //getting the Latitude from the location json
+       that.setState({ currentLongitude:currentLongitude });
+       //Setting state Longitude to re re-render the Longitude Text
+       that.setState({ currentLatitude:currentLatitude });
+       //Setting state Latitude to re re-render the Longitude Text
+    },
+    {timeout:2000}
+    );
+ }
+ componentWillUnmount = () => {
+    Geolocation.clearWatch(this.watchID);
+ }
+ render() {
+   
+  setTimeout(function(){that.setState({timePassed: true})}, 1000);
+    return (
+       <View style = {styles.container}>
+          <Image
+            source={{uri:'https://png.icons8.com/dusk/100/000000/compass.png'}}
+            style={{width: 100, height: 100}}
+          />
+          <Text style = {styles.boldText}>
+             You are Here
+          </Text>
+          <Text style={{justifyContent:'center',alignItems: 'center',marginTop:16}}>
+            Longitude: {this.state.currentLongitude}
+          </Text>
+          <Text style={{justifyContent:'center',alignItems: 'center',marginTop:16}}>
+            Latitude: {this.state.currentLatitude}
+          </Text>
+       </View>
+    )
+ }
+}
+const styles = StyleSheet.create ({
+ container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center',
+    marginTop: 50,
+    padding:16,
+    backgroundColor:'white'
+ },
+ boldText: {
+    fontSize: 30,
+    color: 'black',
+ }
+})
